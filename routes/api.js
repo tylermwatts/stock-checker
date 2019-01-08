ee51c1931fea8d93b3f64f60aeea6ac185e8585a
Glitch (telling-year) <none>
2019-01-08 22:09:48 +0000

    🐴🎬 Checkpoint ./routes/api.js:2521121/148

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,9 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      var query = req.query;
+      console.log(query)
+      // https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo
     });
     
 };
