efbf4e51b20500dfb48829924eccaf0594530ff8
Glitch (telling-year) <none>
2019-01-09 01:03:52 +0000

    🍣👮 Checkpoint ./routes/api.js:2521121/679

--- routes/api.js
+++ routes/api.js
@@ -16,12 +16,22 @@ var request = require('request');
 mongoose.connect(process.env.DB, {useNewUrlParser: true});
 
 module.exports = function (app) {
+  function getStockJson(stock){
+    request('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stock+'&apikey='+process.env.API_KEY, function(err,res){
+      if (err) return JSON.stringify({error: err})
+      return JSON.parse(res.body);
+    })
+  }
 
   app.route('/api/stock-prices')
     .get(function (req, res){
       var query = req.query;
-      var result;
-      
+      console.log(query);
+      var result = getStockJson(query.stock);
+      console.log(result);
+      var returnObj = {stockData: {stock: result['Global Quote']['01. symbol'], price: result['Global Quote']['05. price']}}
+      req.query.like ? returnObj.stockData.likes = 1 : returnObj.stockData.likes = 0
+      res.json(returnObj);
       // https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo
     });
     
