c44a13f7f927cc75bba343b5921d087825593e5b
Glitch (telling-year) <none>
2019-01-09 01:35:52 +0000

    ⏳🛀 Checkpoint ./routes/api.js:2521121/919

--- routes/api.js
+++ routes/api.js
@@ -16,21 +16,24 @@ var request = require('request');
 mongoose.connect(process.env.DB, {useNewUrlParser: true});
 
 module.exports = function (app) {
-  async function getStockJson(stock){
-    var stringifiedJson = await request('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stock+'&apikey='+process.env.API_KEY, function(err,res){
+  function getStockJson(stock){
+    var stringifiedJson = request('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+stock+'&apikey='+process.env.API_KEY, function(err,res){
       if (err) return JSON.stringify(err)
-      console.log(res.body);
-      return JSON.parse(res.body);
+      return res.body;
     })
   }
 
   app.route('/api/stock-prices')
     .get(function (req, res){
       var query = req.query;
-      var result = getStockJson(query.stock)
-      var returnObj = {stockData: {stock: result['Global Quote']['01. symbol'], price: result['Global Quote']['05. price']}}
-      req.query.like ? returnObj.stockData.likes = 1 : returnObj.stockData.likes = 0
-      res.json(returnObj);
+      var returnJson = getStockJson(query.stock)
+        .then(jsonBody=>{
+        var result = JSON.parse(jsonBody)
+        var returnObj = {stockData: {stock: result['Global Quote']['01. symbol'], price: result['Global Quote']['05. price']}}
+        req.query.like ? returnObj.stockData.likes = 1 : returnObj.stockData.likes = 0
+        res.json(returnObj);
+      })
+      
     })// https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo
     
     
