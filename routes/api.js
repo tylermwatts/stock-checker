a6660f4bef1981e4e6140c8ce3cc422f96697f43
Glitch (telling-year) <none>
2019-01-09 02:39:53 +0000

    🎭👅 Checkpoint ./routes/api.js:2521121/1006

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,14 @@ var request = require('request');
 
 mongoose.connect(process.env.DB, {useNewUrlParser: true});
 
+const stockSchema = mongoose.Schema({
+  stock: {type: String, required: true},
+  price: {type: String, required: true},
+  likes: {type: Number, required: true, default: 0}
+})
+
+const Stock = mongoose.model('Stock', stockSchema);
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
@@ -23,8 +31,18 @@ module.exports = function (app) {
       request.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+query.stock+'&apikey='+process.env.API_KEY, function(err, response, body){
         if (err) return res.json({error: err});
         var result = JSON.parse(response.body);
-        var stockObj = {stockData: {stock: result['Global Quote']['01. symbol'], price: result['Global Quote']['05. price']}}
+        var symbol = result['Global Quote']['01. symbol']
+        var price = result['Global Quote']['05. price']
+        Stock.findOne({stock: symbol},(err,stock)=>{
+          if (err) return res.json(err);
+          if (stock){
+            stock.price = price
+          }
+          var stockObj = {stockData: {stock: symbol, price: price}}
+
+        })
         query.like ? stockObj.stockData.likes = 1 : stockObj.stockData.likes = 0;
+        
         res.json(stockObj);
       })
     })// https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo
